f11ebdcee1529b7e99204e8d757966da8a8a1a39
ssoronevich-sergey <s.soronevich@evernode.tech>
2026-09-04 21:29:02 +0300

    Dependency Injection

---
+++ apitest/Dependency/EmailSender.cs
@@ -0,0 +1,9 @@
+﻿namespace apitest.Dependency;
+
+public class EmailSender : IEmailSender
+{
+    public void Send(string to, string text)
+    {
+        Console.WriteLine($"Sending mail to {to}: {text}");
+    }
+}
---
+++ apitest/Dependency/UserNotifier.cs
@@ -0,0 +1,16 @@
+﻿namespace apitest.Dependency;
+
+public class UserNotifier
+{
+    private readonly IEmailSender _emailSender;
+
+    public UserNotifier(IEmailSender emailSender)
+    {
+        _emailSender = emailSender;
+    }
+
+    public void Notify(int userId)
+    {
+        _emailSender.Send("user@mail.com", $"Hello, user {userId}!");
+    }
+}
