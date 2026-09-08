35f1e843196153679e82f48ae7c8cca2415b733c
ssoronevich-sergey <s.soronevich@evernode.tech>
2026-09-08 22:21:23 +0300

    даппер пошел раз

---
+++ apitest/Interfaces/DapperInterfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using apitest.DTO.DapperDTOs;
+using Microsoft.ApplicationInsights;
+
+namespace apitest.Interfaces.DapperInterfaces;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<UserDTO>> GetAllAsync();
+    
+}
---
+++ apitest/Tests/DapperTest.cs
@@ -0,0 +1,16 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace apitest;
+
+public class DapperTest
+{
+    // [Test]
+    public async Task Initialize()
+    {
+        var connectionString = "Data Source=marketplace.db";
+            await using var connection = new SqliteConnection(connectionString);
+            await connection.OpenAsync();
+            await DatabaseInitializer.InitializeAsync(connection);
+    }
+}
