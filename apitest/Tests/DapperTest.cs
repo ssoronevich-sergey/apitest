f9728fa6d11015f4f27978e5a83a0735397d8a01
ssoronevich-sergey <s.soronevich@evernode.tech>
2026-09-09 22:47:48 +0300

    не получается засетапить базу. никак. ругается на no such table: Users. пробовал и наше решение изначальное с сохранением в marketplaceDB и in memory. хожу по кругу, и ошибки те же самые

--- apitest/Tests/DapperTest.cs
+++ apitest/Tests/DapperTest.cs
@@ -1,16 +1,40 @@
-﻿using Dapper;
+﻿using apitest.Interfaces.DapperInterfaces;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Data.Sqlite;
+using FluentAssertions;
 
 namespace apitest;
 
 public class DapperTest
 {
-    // [Test]
-    public async Task Initialize()
+    private readonly TestPrecondition _precondition = new();
+    private SqliteConnection? _connection;
+
+    [SetUp]
+    public async Task Setup()
     {
-        var connectionString = "Data Source=marketplace.db";
-            await using var connection = new SqliteConnection(connectionString);
-            await connection.OpenAsync();
-            await DatabaseInitializer.InitializeAsync(connection);
+        // Используем In-Memory БД
+        _connection = new SqliteConnection("Data Source=:memory:");
+        await _connection.OpenAsync();
+        await DatabaseInitializer.InitializeAsync(_connection);
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        // ✅ Освобождаем ресурсы ПОСЛЕ каждого теста
+        if (_connection != null)
+        {
+            await _connection.DisposeAsync();
+            _connection = null;
+        }
+    }
+
+    [Test]
+    public async Task GetAllUsers()
+    {
+        var repo = _precondition.Provider.GetRequiredService<IUserRepository>();
+        var users = await repo.GetAllAsync();
+        users.Should().HaveCount(15);
     }
 }
